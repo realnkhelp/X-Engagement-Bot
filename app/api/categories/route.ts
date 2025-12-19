a52ebaea21ea33819c4e45f27758b9ccca09000b
realnkhelp <rajababu24553@gmail.com>
2025-12-19 13:21:53 +0530

    update

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
-import { db } from '@/lib/db';
+import { prisma } from '@/lib/db';
 
 export async function GET() {
   try {
-    const [rows] = await db.query('SELECT * FROM categories');
-    return NextResponse.json(rows);
+    // Prisma ka use karke data layenge
+    const categories = await prisma.taskRate.findMany();
+    
+    return NextResponse.json(categories);
   } catch (error) {
     return NextResponse.json({ error: 'Server Error' }, { status: 500 });
   }
